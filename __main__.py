f78256ae7c4dab02ec8c666a951ab90b08e98286
edieguez <dieguezemmanuel@gmail.com>
2019-11-10 16:33:13 -0600

    Basic project structure and dependencies

--- __main__.py
+++ __main__.py
@@ -1,1 +1,5 @@
 #! /usr/bin/env python3
+import email_conf as conf
+
+if __name__ == "__main__":
+    print('{}: {}'.format(conf.from_email, conf.from_email_pass))
---
+++ email_conf.py
@@ -0,0 +1,5 @@
+"""Set these values as environment variables"""
+from os import environ
+
+from_email = environ['from_email']
+from_email_pass = environ['from_email_pass']
