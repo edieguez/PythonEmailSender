9bf281380c6731d67fd70bd7ac7242c40b2001e1
edieguez <dieguezemmanuel@gmail.com>
2019-11-10 18:03:39 -0600

    Added support to send a HTML email

--- __main__.py
+++ __main__.py
@@ -1,5 +1,27 @@
 #! /usr/bin/env python3
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 import email_conf as conf
 
 if __name__ == "__main__":
-    print('{}: {}'.format(conf.from_email, conf.from_email_pass))
+    to_email_list = conf.to_email_list
+    print(to_email_list)
+
+    message = MIMEMultipart('alternative')
+    message['subject'] = 'My custom subject'
+    message['From'] = conf.from_email
+    message['To'] = ','.join(to_email_list)
+
+    html_body = MIMEText('<h1>Simple email from python</h1>', 'html')
+    message.attach(html_body)
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.set_debuglevel(1)
+    server.ehlo()
+    server.starttls()
+    server.login(conf.from_email, conf.from_email_pass)
+
+    server.sendmail(conf.from_email, to_email_list, message.as_string())
+    server.quit()
--- email_conf.py
+++ email_conf.py
@@ -1,5 +1,10 @@
 """Set these values as environment variables"""
 from os import environ
 
+# Must have 'less secure applications' disabled
+# https://www.google.com/settings/security/lesssecureapps
 from_email = environ['from_email']
 from_email_pass = environ['from_email_pass']
+
+# Comma separated values
+to_email_list = environ['to_email_list'].replace(' ', '').split(',')
