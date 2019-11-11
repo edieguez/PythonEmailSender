3a090d7510cfcce43426beffccd6332391ea5e21
edieguez <dieguezemmanuel@gmail.com>
2019-11-10 19:06:53 -0600

    Added basic Jinja2 support

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -7,14 +8,16 @@ import email_conf as conf
 
 if __name__ == "__main__":
     to_email_list = conf.to_email_list
-    print(to_email_list)
 
     message = MIMEMultipart('alternative')
     message['subject'] = 'My custom subject'
     message['From'] = conf.from_email
     message['To'] = ','.join(to_email_list)
 
-    html_body = MIMEText('<h1>Simple email from python</h1>', 'html')
+    template = conf.get_template('notification_email.html')
+    html_text = template.render(message='Your message goes here')
+
+    html_body = MIMEText(html_text, 'html')
     message.attach(html_body)
 
     server = smtplib.SMTP('smtp.gmail.com:587')
--- email_conf.py
+++ email_conf.py
@@ -1,5 +1,6 @@
 """Set these values as environment variables"""
-from os import environ
+from os import environ, path
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 # Must have 'less secure applications' disabled
 # https://www.google.com/settings/security/lesssecureapps
@@ -8,3 +9,12 @@ from_email_pass = environ['from_email_pass']
 
 # Comma separated values
 to_email_list = environ['to_email_list'].replace(' ', '').split(',')
+
+# Jinja template engine setup
+env = Environment(
+    loader=FileSystemLoader(path.join(path.dirname(path.abspath(__file__)), 'templates')),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+def get_template(template_name):
+    return env.get_template(template_name)
